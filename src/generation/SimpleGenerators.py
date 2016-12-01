495cb23bf151de3ac4a801ee6177ae8d47b4dfb0
Gregory Gay <greg@greggay.com>
2016-12-01 15:59:49 -0500

    Adds short/long unsigned/signed int/double/float

--- src/generation/SimpleGenerators.py
+++ src/generation/SimpleGenerators.py
@@ -3,21 +3,29 @@ import string
 
 # Simple integer generator.
 class IntGenerator():
-    minInt = -2147483647
-    maxInt = 2147483647
+    minInt = -32767
+    maxInt = 32767
 
     def generate(self):
         return str(random.randint(self.minInt,self.maxInt))
 
 # Simple character generator.
 class CharGenerator():
-    minChar=0
-    maxChar=255
+    minChar = 0
+    maxChar = 255
 
     def generate(self):
         numRepresentation=random.randint(self.minChar,self.maxChar)
         #charRepresentation=chr(numRepresentation)
         return str(numRepresentation)
+
+# Simple floating-point generator.
+class FloatGenerator():
+    minFloat = -3.4E+38
+    maxFloat = 3.4E+38
+   
+    def generate(self):
+        return str(random.uniform(self.minFloat,self.maxFloat))
 
 # Returns an empty string for void (no argument) input
 class VoidGenerator():
@@ -47,33 +55,137 @@ class AvailableGenerator():
 class GeneratorFactory():
     available = []
     ig = IntGenerator()
+    defaultIntMin = -32767
+    defaultIntMax = 32767
+    shortIntMin = -32767
+    shortIntMax = 32767
+    longIntMin = -2147483647
+    longIntMax = 2147483647
+    longLongIntMin = -9223372036854775807
+    longLongIntMax = 9223372036854775807
+    unsignedIntMin = 0
+    unsignedIntMax = 65535
+    unsignedShortMax = 65535
+    unsignedLongMax = 4294967295
+    unsignedLongLongMax = 18446744073709551615
     cg = CharGenerator()
+    defaultCharMin = 0
+    defaultCharMax = 255
+    signedCharMin = -127
+    signedCharMax = 127
+    fg = FloatGenerator()
+    defaultFloatMin = 1.2E-38
+    defaultFloatMax = 3.4E+38
+    doubleMin = 2.3E-308
+    doubleMax = 1.7E+308
+    longDoubleMin = 3.4E-4932
+    longDoubleMax = 1.1E+4932
     vg = VoidGenerator()
     ag = AvailableGenerator()
 
     def generate(self,typeToGenerate):
-        if typeToGenerate == "int":
-            # At a set probability, use an available variable instead of generating a new value.
-            if random.random() < 0.25:
-                choice = self.ag.generate(self.available,typeToGenerate)
-                if choice != "":
-                    return choice
-                else:
-                    return self.ig.generate()
-            else:
-                return self.ig.generate()
+        # At a set probability, use an available variable instead of generating a new value.
+        if random.random() < 0.25:
+            choice = self.ag.generate(self.available,typeToGenerate)
+            if choice != "":
+                return choice
+
+        if typeToGenerate == "int" or typeToGenerate == "signed int": 
+            return self.ig.generate()
+        elif typeToGenerate == "short" or typeToGenerate == "short int" or \
+            typeToGenerate == "signed short" or typeToGenerate == "signed short int":
+            self.ig.minInt = self.shortIntMin
+            self.ig.maxInt = self.shortIntMax
+
+            value = self.ig.generate()
+
+            self.ig.minInt = self.defaultIntMin
+            self.ig.maxInt = self.defaultIntMax
+
+            return value
+        elif typeToGenerate == "unsigned short" or typeToGenerate == "unsigned short int": 
+            self.ig.minInt = self.unsignedIntMin
+            self.ig.maxInt = self.unsignedShortMax
+
+            value = self.ig.generate()
+
+            self.ig.minInt = self.defaultIntMin
+            self.ig.maxInt = self.defaultIntMax
+
+            return value
+        elif typeToGenerate == "long" or typeToGenerate == "long int" or \
+            typeToGenerate == "signed long" or typeToGenerate == "signed long int":
+            self.ig.minInt = self.longIntMin
+            self.ig.maxInt = self.longIntMax
+
+            value = self.ig.generate()
+
+            self.ig.minInt = self.defaultIntMin
+            self.ig.maxInt = self.defaultIntMax
+
+            return value
+        elif typeToGenerate == "unsigned long" or typeToGenerate == "unsigned long int": 
+            self.ig.minInt = self.unsignedIntMin
+            self.ig.maxInt = self.unsignedLongMax
+
+            value = self.ig.generate()
+
+            self.ig.minInt = self.defaultIntMin
+            self.ig.maxInt = self.defaultIntMax
+
+            return value
+        elif typeToGenerate == "long long" or typeToGenerate == "long long int" or \
+            typeToGenerate == "signed long long" or typeToGenerate == "signed long long int":
+            self.ig.minInt = self.longLongIntMin
+            self.ig.maxInt = self.longLongIntMax
+
+            value = self.ig.generate()
+
+            self.ig.minInt = self.defaultIntMin
+            self.ig.maxInt = self.defaultIntMax
+
+            return value
+        elif typeToGenerate == "unsigned long long" or typeToGenerate == "unsigned long long int": 
+            self.ig.minInt = self.unsignedIntMin
+            self.ig.maxInt = self.unsignedLongLongMax
+
+            value = self.ig.generate()
+
+            self.ig.minInt = self.defaultIntMin
+            self.ig.maxInt = self.defaultIntMax
+
+            return value
         elif typeToGenerate == "char" or typeToGenerate == "unsigned char":
             return self.cg.generate()
-
         elif typeToGenerate == "signed char":
-            self.cg.minChar = -128
-            self.cg.maxChar = 127
+            self.cg.minChar = self.signedCharMin
+            self.cg.maxChar = self.signedCharMax
   
             value = self.cg.generate()
 
-            self.cg.minChar = 0
-            self.cg.maxChar = 255
+            self.cg.minChar = self.defaultCharMin
+            self.cg.maxChar = self.defaultCharMax
 
+            return value
+        elif typeToGenerate == "float":
+            return self.fg.generate()
+        elif typeToGenerate == "double":
+            self.fg.minFloat = self.doubleMin
+            self.fg.maxFloat = self.doubleMax
+
+            value = self.fg.generate()
+
+            self.fg.minFloat = self.defaultFloatMin
+            self.fg.maxFloat = self.defaultFloatMax
+            return value
+        elif typeToGenerate == "long double":
+            self.fg.minFloat = self.longDoubleMin
+            self.fg.maxFloat = self.longDoubleMax
+
+            value = self.fg.generate()
+
+            self.fg.minFloat = self.defaultFloatMin
+            self.fg.maxFloat = self.defaultFloatMax
             return value
         elif typeToGenerate == "void" or typeToGenerate == "":
             return self.vg.generate()
